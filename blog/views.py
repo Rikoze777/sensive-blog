0e6d9fe7edae5a2af2783ba9100312311ee1038a
Rikoze777 <61386361+Rikoze777@users.noreply.github.com>
2022-11-02 15:48:16 +0300

    reduce db queries number

--- blog/views.py
+++ blog/views.py
@@ -43,10 +43,10 @@ def serialize_tag(tag):
 
 
 def index(request):
-    most_popular_posts = Post.objects.popular().prefetch_related('author')[:5].fetch_with_comments_count()
+    most_popular_posts = Post.objects.popular().prefetch_related('author', 'tags')[:5].fetch_with_comments_count()
 
     fresh_posts = (Post.objects.order_by("-published_at")
-                   .prefetch_related("author")
+                   .prefetch_related('author', 'tags')
                    .annotate(comments_count=Count("comments", distinct=True))[:5])
     most_fresh_posts = list(fresh_posts)[:5]
 
@@ -91,7 +91,7 @@ def post_detail(request, slug):
 
     most_popular_tags = Tag.objects.popular()[-5:]
 
-    most_popular_posts = Post.objects.popular().prefetch_related('author')[:5].fetch_with_comments_count()
+    most_popular_posts = Post.objects.popular().prefetch_related('author', 'tags')[:5].fetch_with_comments_count()
 
     context = {
         'post': serialized_post,
@@ -108,7 +108,7 @@ def tag_filter(request, tag_title):
 
     most_popular_tags = Tag.objects.popular()[-5:]
 
-    most_popular_posts = Post.objects.popular().prefetch_related('author')[:5].fetch_with_comments_count()
+    most_popular_posts = Post.objects.popular().prefetch_related('author', 'tags')[:5].fetch_with_comments_count()
 
     related_posts = tag.posts.annotate(
         comments_count=Count('comments')
